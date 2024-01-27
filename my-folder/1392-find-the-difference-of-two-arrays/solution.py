950ea66a5701e66b7d29c6fb70714e9654e8fb06
John Lin <120153300+codecreator127@users.noreply.github.com>
2024-01-27 07:42:59 +0000

    [LeetCode Sync] Runtime - 152 ms (16.64%), Memory - 17.1 MB (100.00%)

--- my-folder/1392-find-the-difference-of-two-arrays/solution.py
+++ my-folder/1392-find-the-difference-of-two-arrays/solution.py
@@ -1,15 +1,23 @@
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        # brute force
+        
+        out1 = list(set(nums1) - set(nums2))
+        out2 = list(set(nums2) - set(nums1))
 
-        output = [[],[]]
-        for num in nums1:
-            if num not in nums2 and num not in output[0]:
-                output[0].append(num)
-
-        for num in nums2:
-            if num not in nums1 and num not in output[1]:
-                output[1].append(num)
+        return [out1, out2]
 
 
-        return output
+
+        # # brute force
+
+        # output = [[],[]]
+        # for num in nums1:
+        #     if num not in nums2 and num not in output[0]:
+        #         output[0].append(num)
+
+        # for num in nums2:
+        #     if num not in nums1 and num not in output[1]:
+        #         output[1].append(num)
+
+
+        # return output
