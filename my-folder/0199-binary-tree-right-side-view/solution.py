67ad6f10e890528b8ead7d29ddf0ac1567caf7ec
John Lin <120153300+codecreator127@users.noreply.github.com>
2024-02-21 07:26:50 +0000

    [LeetCode Sync] Runtime - 27 ms (0.73%), Memory - 16.5 MB (100.00%)

---
+++ my-folder/0199-binary-tree-right-side-view/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        #do BFS, then return only the right values
+        ans = []
+        level = 0
+        def helper(root, level):
+            if not root:
+                return
+            if len(ans) == level:
+                ans.append(root.val)
+
+            helper(root.right, level + 1)
+            helper(root.left, level + 1)
+
+
+        helper(root, level)
+        return ans
